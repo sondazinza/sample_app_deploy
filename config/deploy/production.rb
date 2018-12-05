f8181ae5f7f6e7aa1c2ff2928353ea7f4bc5c8e8
Zinza Dev <dev@zinza.com.vn>
2018-12-05 16:31:59 +0700

    Fix config cap 6

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ set :rails_env, 'production'
 set :branch, "master"
 
 server "45.118.134.212", user: "root", roles: %w{web, app, db}, primary: true
-set :deploy_to, "/var/www/apps/#{fetch(:application)}"
+# set :deploy_to, "/var/www/apps/#{fetch(:application)}"
 
 
 set :ssh_options, { forward_agent: true, user: "root", keys: %w(~/.ssh/id_rsa.pub) }
