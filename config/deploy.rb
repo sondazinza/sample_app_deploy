99f9a4ac35bb0fe8098601c559cfa8550780cc73
Zinza Dev <dev@zinza.com.vn>
2018-12-05 17:57:30 +0700

    Fix config cap 15

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,27 +2,25 @@
 lock "~> 3.11.0"
 
 set :application, "sample_app_deploy"
-set :repo_url, "git@github.com:sondazinza/sample_app_deploy.git"
-
-set :deploy_to, "/home/sonda/sample_app_deploy"
-
-set :branch, "master"
-
-# set :pty, false
-set :format, :pretty
-
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :repo_url, "git@github.com:sondazinza/sample_app_deploy.git"
+set :ssh_options, { forward_agent: true, user: "root", keys: %w(~/.ssh/id_rsa.pub) }
+set :deploy_to, "/home/sonda/sample_app_deploy"
+set :branch, "master"
+
+set :format, :pretty
+
+# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+# set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+# set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+# set :puma_access_log, "#{release_path}/log/puma.error.log"
+# set :puma_error_log,  "#{release_path}/log/puma.access.log"
+# set :puma_preload_app, true
+# set :puma_worker_timeout, nil
+# set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 
 set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -35,94 +33,51 @@ namespace :puma do
       execute "mkdir #{shared_path}/tmp/pids -p"
     end
   end
+
+  before :start, :make_dirs
 end
 
-# namespace :deploy do
-#   desc "Make sure local git is in sync with remote."
-#   task :check_revision do
-#     on roles(:app) do
-#       unless `git rev-parse HEAD` == `git rev-parse origin/master`
-#         puts "WARNING: HEAD is not the same as origin/master"
-#         puts "Run `git push` to sync changes."
-#       exit
-#     end
-#   end
-# end
-
-desc 'Initial Deploy'
-  task :initial do
-  on roles(:app) do
-    before 'deploy:restart', 'puma:start'
-    invoke 'deploy'
-  end
-end
-
-desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-    invoke 'puma:restart'
-  end
-end
-
-
-desc "Run rake db:seed on a remote server."
-task :seed do
-  on roles (:app) do
-    within release_path do
-      with rails_env: fetch(:rails_env) do
-        execute :rake, "db:seed"
-      end
-    end
-  end
-end
-
-# ps aux | grep puma    # Get puma pid
-
-# kill -s SIGUSR2 pid   # Restart puma
-
-# kill -s SIGTERM pid   # Stop puma
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
 namespace :deploy do
+  after :finishing, 'deploy:cleanup'
+  after 'deploy:publishing', 'deploy:restart'
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       within release_path do
-        # execute :rake, 'cache:clear'
-        execute :puma
-        # invoke 'puma:restart'
+        execute :rake, 'tmp:clear'
       end
     end
   end
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,6 @@ server "45.118.134.212", user: "root", roles: %w{web, app, db}, primary: true
 # set :deploy_to, "/var/www/apps/#{fetch(:application)}"
 
 
-set :ssh_options, { forward_agent: true, user: "root", keys: %w(~/.ssh/id_rsa.pub) }
 set :stage, :production
 set :branch, :master
 
