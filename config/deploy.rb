c84431fbdef496fbbeeead7eacb64de35d90926b
Zinza Dev <dev@zinza.com.vn>
2018-12-06 12:10:34 +0700

    ss

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,6 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
+
 # Change these
 server "45.118.134.212", user: "root", roles: %w{web, app, db}, primary: true
 
@@ -13,7 +16,7 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/sonda/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind,       "unix:///#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
