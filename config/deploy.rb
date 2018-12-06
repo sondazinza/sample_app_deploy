8800049b6c4c4aba315bc7726f6b7382f189d923
Zinza Dev <dev@zinza.com.vn>
2018-12-06 10:28:11 +0700

    Fix config cap 21

--- config/deploy.rb
+++ config/deploy.rb
@@ -76,7 +76,10 @@ namespace :deploy do
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
+  after :finishing, :restart
 end
+
+after 'deploy:publishing', 'deploy:initial'
 
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
